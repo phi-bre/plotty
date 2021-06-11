c8d50bbad32fc333291883178f3ce99d49bfe8fe
Philip Breuer <philip.breuer@pm.me>
2021-06-11 20:43:36 +0200

    First working shader

--- src/lib/mandelbrot.js
+++ src/lib/mandelbrot.js
@@ -5,18 +5,18 @@ export class Mandelbrot {
   constructor(canvas) {
     this.blub = true;
     this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
-    // this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.0000001, 1000);
-    this.camera = new THREE.OrthographicCamera(
-      window.innerWidth / -2,
-      window.innerWidth / 2,
-      window.innerHeight / -2,
-      window.innerHeight / 2,
-      Number.MIN_VALUE,
-      Number.MAX_VALUE,
-    );
-    // this.camera.position.set(0, 0, 100);
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.0000001, 1000);
+    // this.camera = new THREE.OrthographicCamera(
+    //   window.innerWidth / -2,
+    //   window.innerWidth / 2,
+    //   window.innerHeight / -2,
+    //   window.innerHeight / 2,
+    //   Number.MIN_VALUE,
+    //   Number.MAX_VALUE,
+    // );
+    this.camera.position.set(0, 0, 100);
     this.camera.lookAt(0, 0, 0);
-    this.camera.zoom = 1;
+    // this.camera.zoom = 1;
     const controls = new THREE.OrbitControls(this.camera, canvas);
     controls.enableRotate = true;
     controls.screenSpacePanning = true;
@@ -37,6 +37,11 @@ export class Mandelbrot {
     // // this.mesh.scale.addScalar(100);
     // this.scene.add(this.mesh);
 
+    // const geometry = new THREE.SphereGeometry(5, 32, 32);
+    // const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    // const sphere = new THREE.Mesh(geometry, material);
+    // this.scene.add(sphere);
+
     this.material = new THREE.ShaderMaterial({
       uniforms: {
         resolution: {
@@ -53,25 +58,23 @@ export class Mandelbrot {
       fragmentShader: `
         uniform vec2 resolution;
         void main() {
-          const float scale = 1.0;
+          const float scale = 300.0;
           const int depth = 100;
           int iteration = 0;
+          vec2 location = ((gl_FragCoord.xy - resolution / 2.0) /*+ vec2(viewMatrix)*/) / scale;
+          
+          if (location.x < -2.5 || location.x > 1.0 || location.y < -1.0 || location.y > 2.0) return;
+          
           float zr = 0.0;
           float zi = 0.0;
-          float x = (gl_FragCoord.x - resolution.x / 2.0) / scale;
-          float y = (gl_FragCoord.y - resolution.y / 2.0) / scale;
-          for (int i = 0; i < 100; i++) {
-            if (zr * zr + zi * zi <= 4.0) {
-              float z = zr * zr - zi * zi + x;
-              zi = 2.0 * zr * zi + y;
-              zr = z;
-              iteration = i;
-            }
+          for (int i = 0; i < 100 && (zr * zr + zi * zi <= 4.0); i++) {
+            float z = zr * zr - zi * zi + location.x;
+            zi = 2.0 * zr * zi + location.y;
+            zr = z;
+            iteration = i;
           }
           float c = 1.0 / float(depth) * float(iteration);
-          if (x < resolution.x / 4.0 && y < 10.0 && x > 0.0 && y > 0.0) {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-          }
+          gl_FragColor = vec4(c, c, c, 1.0);
         }
       `,
     });
@@ -79,10 +82,11 @@ export class Mandelbrot {
     this.scene.add(this.mesh);
 
     window.addEventListener('resize', this.resize.bind(this), false);
-    // controls.addEventListener('change', () => {
-    //   this.update();
-    //   // this.renderer.render(this.scene, this.camera);
-    // });
+    controls.addEventListener('change', () => {
+      this.render();
+      // this.update();
+      // this.renderer.render(this.scene, this.camera);
+    });
     this.resize();
     this.render();
     // this.update();
