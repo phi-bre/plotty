3421f7ed7e81acebd3633850b13c4d05de039fbc
Philip Breuer <philip.breuer@pm.me>
2021-06-15 21:16:40 +0200

    Fix deployment

--- src/routes/bezier.svelte
+++ src/routes/bezier.svelte
@@ -4,18 +4,21 @@
 
 <script>
   import { onMount } from "svelte";
+  import { browser } from "$app/env";
 
   let canvas, bezier;
 
-  onMount(async () => {
-    const { Bezier } = await import("../lib/bezier");
-    bezier = new Bezier(canvas);
+  if (browser) {
+    onMount(async () => {
+      const { Bezier } = await import("../lib/bezier");
+      bezier = new Bezier(canvas);
 
-    // setInterval(() => {
-    //   bezier.parameter = (bezier.parameter + 0.001) % 1;
-    //   bezier.update();
-    // }, 10);
-  });
+      // setInterval(() => {
+      //   bezier.parameter = (bezier.parameter + 0.001) % 1;
+      //   bezier.update();
+      // }, 10);
+    });
+  }
 </script>
 
 <canvas bind:this={canvas} />
