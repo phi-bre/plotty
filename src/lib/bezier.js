fdc3660fdbff9c6fa2150fd13a53ba9e975748ca
Philip Breuer <philip.breuer@pm.me>
2021-06-19 14:09:04 +0200

    Add point controls

--- src/lib/bezier.js
+++ src/lib/bezier.js
@@ -47,7 +47,9 @@ function bezier(curve, precision) {
 }
 
 export class Bezier {
-  parameter = 0;
+  controlObjects = [];
+  mouse = new THREE.Vector2();
+  parameter = 0.5;
   bezier = [
     new THREE.Vector3(0.30959752321981426, 0.7575757575757576),
     new THREE.Vector3(0.2777777777777778, 0.5),
@@ -60,43 +62,63 @@ export class Bezier {
     this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.0000001, 1000);
     this.camera.position.set(1.0411534907119235, 0.6965274187556713, 1.0122094541157602);
-    this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+    this.raycaster = new THREE.Raycaster();
 
+    this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
     this.controls.addEventListener('change', () => {
       this.render();
     });
+
+    this.transform = new THREE.TransformControls(this.camera, canvas);
+    this.transform.addEventListener('objectChange', () => {
+      this.transform.object.point.copy(this.transform.object.position);
+      this.update();
+    });
+    this.scene.add(this.transform);
+
     window.addEventListener('resize', () => {
       this.resize();
     });
+    window.addEventListener('mousemove', (event) => {
+      this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+      this.move();
+    });
+    window.addEventListener('click', (event) => {
+      this.click();
+    });
 
     this.resize();
+    this.create();
     this.update();
   }
 
-  update() {
-    this.scene.remove(...this.scene.children);
-
-    const curveGeometry = new THREE.LineGeometry().fromLine({
-      geometry: new THREE.BufferGeometry().setFromPoints(bezier(this.bezier, 100)),
-    });
+  create() {
+    // BEZIER CURVE
+    if (this.curveObject) this.scene.remove(this.curveObject);
+    const curveGeometry = new THREE.LineGeometry();
     const curveMaterial = new THREE.LineMaterial({
       resolution: new THREE.Vector2(window.innerWidth, window.innerHeight),
       linewidth: 4,
       color: '#3498db',
     });
-    const curve = new THREE.LineSegments2(curveGeometry, curveMaterial);
+    this.curveObject = new THREE.LineSegments2(curveGeometry, curveMaterial);
+    this.scene.add(this.curveObject);
 
-    const controls = this.bezier.map((point) => {
+    // CONTROL POINTS
+    if (this.controlObjects) this.scene.remove(...this.controlObjects);
+    this.controlObjects = this.bezier.map((point) => {
       const sphereGeometry = new THREE.SphereGeometry(0.005);
       const sphereMaterial = new THREE.MeshBasicMaterial({ color: '#8b8b8b' });
       const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-      sphere.position.copy(point);
+      sphere.point = point;
       return sphere;
     });
+    this.scene.add(...this.controlObjects);
 
-    const outlineGeometry = new THREE.LineGeometry().fromLine({
-      geometry: new THREE.BufferGeometry().setFromPoints(this.bezier),
-    });
+    // OUTLINE
+    if (this.outlineObject) this.scene.remove(this.outlineObject);
+    const outlineGeometry = new THREE.LineGeometry();
     const outlineMaterial = new THREE.LineMaterial({
       resolution: new THREE.Vector2(window.innerWidth, window.innerHeight),
       linewidth: 3,
@@ -104,14 +126,14 @@ export class Bezier {
       transparent: true,
       opacity: 0.5,
     });
-    const outline = new THREE.LineSegments2(outlineGeometry, outlineMaterial);
+    this.outlineObject = new THREE.LineSegments2(outlineGeometry, outlineMaterial);
+    this.scene.add(this.outlineObject);
 
-    const triangulated = triangulate(this.bezier, this.parameter);
-    const sliders = triangulated.sliders.map((slider, i) => {
+    // SLIDERS
+    if (this.sliderObjects) this.scene.remove(...this.sliderObjects);
+    this.sliderObjects = this.bezier.map((point, i) => {
       const color = colors[i % colors.length];
-      const sliderGeometry = new THREE.LineGeometry().fromLine({
-        geometry: new THREE.BufferGeometry().setFromPoints(slider),
-      });
+      const sliderGeometry = new THREE.LineGeometry();
       const sliderMaterial = new THREE.LineMaterial({
         color,
         resolution: new THREE.Vector2(window.innerWidth, window.innerHeight),
@@ -120,19 +142,71 @@ export class Bezier {
         opacity: 0.5,
       });
       const sliderMesh = new THREE.LineSegments2(sliderGeometry, sliderMaterial);
-
-      for (const point of slider) {
-        const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.005), new THREE.MeshBasicMaterial({ color }));
-        sphere.position.copy(point);
-        sliderMesh.add(sphere);
-      }
-
+      // slider.forEach(point => {
+      //   const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.005), new THREE.MeshBasicMaterial({ color }));
+      //   sphere.position.copy(point);
+      //   sliderMesh.add(sphere);
+      // });
       return sliderMesh;
     });
-    const parameter = new THREE.Mesh(new THREE.SphereGeometry(0.01), new THREE.MeshBasicMaterial({ color: 'red' }));
-    parameter.position.copy(triangulated);
+    this.scene.add(...this.sliderObjects);
 
-    this.scene.add(curve, outline, parameter, ...sliders, ...controls);
+    // PARAMETER POINT
+    if (this.parameterObject) this.scene.remove(this.parameterObject);
+    const parameterGeometry = new THREE.SphereGeometry(0.01);
+    const parameterMaterial = new THREE.MeshBasicMaterial({ color: 'red' });
+    this.parameterObject = new THREE.Mesh(parameterGeometry, parameterMaterial);
+    this.scene.add(this.parameterObject);
+  }
+
+  update() {
+    const triangulated = triangulate(this.bezier, this.parameter);
+
+    this.curveObject.geometry.fromLine({
+      geometry: new THREE.BufferGeometry().setFromPoints(bezier(this.bezier, 100)),
+    });
+
+    this.controlObjects.forEach((point, i) => {
+      point.position.copy(this.bezier[i]);
+    });
+
+    this.outlineObject.geometry.fromLine({
+      geometry: new THREE.BufferGeometry().setFromPoints(this.bezier),
+    });
+
+    this.sliderObjects.forEach((slider, i) => {
+      if (!triangulated.sliders[i]) return;
+      slider.geometry.fromLine({
+        geometry: new THREE.BufferGeometry().setFromPoints(triangulated.sliders[i]),
+      });
+    });
+
+    this.parameterObject.position.copy(triangulated);
+
+    this.render();
+  }
+
+  click() {
+    const [point] = this.raycaster.intersectObjects(this.controlObjects);
+    if (point) {
+      this.transform.attach(point.object);
+      this.controls.enabled = false;
+    } else if (this.transform.object) {
+      this.transform.detach();
+      this.controls.enabled = true;
+    } else {
+      this.bezier.push(new THREE.Vector3(this.mouse.x, this.mouse.y));
+      this.create();
+      this.update();
+    }
+    this.render();
+  }
+
+  move() {
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    this.movingPoint?.object.material.color.set(0xffffff);
+    this.movingPoint = this.raycaster.intersectObjects(this.controlObjects)[0];
+    this.movingPoint?.object.material.color.set(0xff0000);
     this.render();
   }
 
--- src/routes/bezier.svelte
+++ src/routes/bezier.svelte
@@ -13,25 +13,13 @@
       const { Bezier } = await import("../lib/bezier");
       bezier = new Bezier(canvas);
 
-      // setInterval(() => {
-      //   bezier.parameter = (bezier.parameter + 0.001) % 1;
-      //   bezier.update();
-      // }, 10);
+      setInterval(() => {
+        bezier.parameter = (bezier.parameter + 0.001) % 1;
+        bezier.update();
+      }, 10);
     });
   }
 </script>
 
 <canvas bind:this={canvas} />
-{#if bezier}
-  <div class="menu">
-    <input type="range" min="0" max="1" step="0.01" bind:value={bezier.parameter} on:input={() => bezier.update()}>
-  </div>
-{/if}
 
-<style>
-    .menu {
-        position: fixed;
-        top: 10px;
-        right: 10px;
-    }
-</style>
