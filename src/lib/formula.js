348526625e1df44222a1dccd65ee74c0262aad5b
Philip Breuer <philip.breuer@pm.me>
2022-02-06 10:08:42 +0100

    Add glsl renderer

--- src/lib/formula.js
+++ src/lib/formula.js
@@ -1,7 +1,7 @@
-import { writable } from "svelte/store";
-import * as math from "mathjs";
-import katex from "katex";
-import * as THREE from "./three";
+import { writable } from 'svelte/store';
+import * as math from 'mathjs';
+import katex from 'katex';
+import * as THREE from './three';
 
 export default function (canvas) {
   const store = writable({
@@ -11,8 +11,6 @@ export default function (canvas) {
     latex: '',
     scope: {},
   });
-
-  setFormula('sin(x) + a');
 
   const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
   const camera = new THREE.OrthographicCamera(
@@ -35,46 +33,73 @@ export default function (canvas) {
   grid.rotateX(Math.PI / 2);
   scene.add(grid);
 
-  const material = new THREE.LineBasicMaterial({ color: 0x5dadff, linewidth: 2 });
+  const material = new THREE.ShaderMaterial({
+    fragmentShader: `
+      void main() {
+        gl_FragColor = vec4(0.365, 0.678, 1, 0);
+      }
+    `,
+  });
+
+  // const material = new THREE.LineBasicMaterial({ color: 0x5dadff, linewidth: 2 });
   const geometry = new THREE.BufferGeometry();
+  const POINT_COUNT = 100000;
+  const points = [];
+  for (let i = 0; i < POINT_COUNT; i++) {
+    points.push(new THREE.Vector3(i - POINT_COUNT / 2, 0));
+  }
+  geometry.setFromPoints(points);
+
   const mesh = new THREE.Line(geometry, material);
   mesh.frustumCulled = false;
   scene.add(mesh);
 
-  controls.addEventListener('change', () => store.update(e => e));
+  controls.addEventListener('change', () => store.update((e) => e));
   window.addEventListener('resize', resize);
   resize();
+  setFormula('tan(x)');
 
   function resize() {
-    const parent = canvas.parentElement.getBoundingClientRect();
-    canvas.width = parent.width;
-    canvas.height = parent.height;
-    camera.aspect = canvas.width / canvas.height;
+    const size = canvas.getBoundingClientRect();
+    canvas.width = size.width;
+    canvas.height = size.height;
+    camera.left = size.width / -1;
+    camera.right = size.width / +1;
+    camera.top = size.height / -1;
+    camera.bottom = size.height / +1;
     camera.updateProjectionMatrix();
-    // renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(canvas.width, canvas.height);
-    store.update(s => s);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(size.width, size.height);
+    store.update((s) => s);
   }
 
   store.subscribe((state) => {
     if (!state.compiled) return;
+    // const lx = camera.position.x - -camera.left / camera.zoom;
+    // const rx = camera.position.x + +camera.right / camera.zoom;
+    // points.forEach((point, index) => {
+    //   point.x = lx + (rx - lx) * index / points.length;
+    // });
+    // geometry.setFromPoints(points);
+    material.uniforms.zoom = { value: camera.zoom };
+    renderer.render(scene, camera);
 
-    try {
-      const lx = camera.position.x - -camera.left / camera.zoom;
-      const rx = camera.position.x + +camera.right / camera.zoom;
-      const step = 1 / camera.zoom;
-      const points = [];
-
-      for (let x = lx; x <= rx; x += step) {
-        const y = -state.compiled.evaluate({ ...state.scope, x });
-        points.push(new THREE.Vector3(x, y));
-      }
-
-      geometry.setFromPoints(points);
-      renderer.render(scene, camera);
-    } catch (e) {
-      console.error(e);
-    }
+    // try {
+    //   const lx = camera.position.x - -camera.left / camera.zoom;
+    //   const rx = camera.position.x + +camera.right / camera.zoom;
+    //   const step = 1 / camera.zoom;
+    //   const points = [];
+    //
+    //   for (let x = lx; x <= rx; x += step) {
+    //     const y = -state.compiled.evaluate({ ...state.scope, x });
+    //     points.push(new THREE.Vector3(x, y));
+    //   }
+    //
+    //   geometry.setFromPoints(points);
+    //   renderer.render(scene, camera);
+    // } catch (e) {
+    //   console.error(e);
+    // }
   });
 
   function setFormula(formula) {
@@ -86,14 +111,44 @@ export default function (canvas) {
         displayMode: true,
         output: 'html',
       });
-      const variables = parsed.filter((node) => node.isSymbolNode && node.name.length === 1 && node.name !== 'x')
+      const variables = parsed
+        .filter((node) => node.isSymbolNode && node.name.length === 1 && node.name !== 'x')
         .map((node) => [node.name, 0])
         .sort();
       const scope = Object.fromEntries(variables);
+      const glsl = parsed.toString({
+        handler(node, options) {
+          if (node.type === 'ConstantNode') {
+            return `float(${node.value})`;
+          } else if (node.type === 'OperatorNode') {
+            if (node.op === '^') {
+              return `pow(${node.args[0].toString(options)}, ${node.args[1].toString(options)})`;
+            } else {
+              return `${node.args[0].toString(options)} ${node.op} ${node.args[1].toString(options)}`;
+            }
+          }
+          return node.toString();
+        },
+      });
+      material.vertexShader = `
+        uniform float zoom;
+        ${variables.map(([name]) => `uniform float ${name};`).join('\n')}
+        void main() {
+          float x = position.x * (1.0 / zoom) + cameraPosition.x;
+          float y = (${glsl});
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(x, -y, 0, 1.0);
+        }
+      `;
+      mesh.material = material;
+      mesh.material.needsUpdate = true;
 
-      store.update(state => {
+      store.update((state) => {
         Object.keys(scope).forEach((key) => {
           state.scope[key] ??= scope[key];
+          material.uniforms[key] = {
+            type: 'float',
+            value: state.scope[key],
+          };
         });
 
         state.formula = formula;
@@ -103,22 +158,21 @@ export default function (canvas) {
 
         return state;
       });
-
     } catch (e) {
       console.error(e);
     }
   }
 
   function setScope(name, value) {
-    store.update(state => {
-      state.scope[name] = value;
+    store.update((state) => {
+      state.scope[name] = math.round(value, 2);
+      material.uniforms[name] = state.scope[name];
+      mesh.material.needsUpdate = true;
       return state;
     });
   }
 
-  function destroy() {
-
-  }
+  function destroy() {}
 
   return { store, setFormula, setScope, destroy };
 }
--- src/routes/formula.svelte
+++ src/routes/formula.svelte
@@ -14,15 +14,15 @@
     Tile,
   } from 'carbon-components-svelte';
   import 'katex/dist/katex.css';
-  import { browser } from "$app/env";
-  import { onDestroy, onMount } from "svelte";
+  import { browser } from '$app/env';
+  import { onDestroy, onMount } from 'svelte';
 
   let canvas;
   let formula;
   let store;
   if (browser) {
     onMount(async () => {
-      formula = (await import("$lib/formula")).default(canvas);
+      formula = (await import('$lib/formula')).default(canvas);
       store = formula.store;
     });
     onDestroy(() => formula.destroy());
@@ -36,44 +36,41 @@
 <Header href="/" company="plotty —" platformName="Formula" />
 
 <Content style="background-color: transparent">
-  <Grid fullWidth>
-    <Row>
-      <Column style="min-height: 80vh">
-        <canvas bind:this={canvas} style="width: 100%; height: 100%" />
-      </Column>
+  <div style="display: flex">
+    <canvas bind:this={canvas} style="flex-grow: 1; min-height: 90vh" />
+    <div style="margin-left: 16px; min-width: 340px">
       {#if $store}
-        <Column style="max-width: 340px">
-          <TextInput
-            id="formula"
-            labelText="Formula"
-            invalidText="Your formula seems invalid"
-            placeholder="2 + 2"
-            value={$store.formula}
-            on:input={(e) => formula.setFormula(e.target.value)}
-          />
-          <Tile style="display: flex; justify-content: center; align-items: center">
-            {@html $store.latex}
-          </Tile>
-          <br />
-          <Form>
-            <FormGroup legendText="Variables {Object.keys($store.scope).join()}">
-              {#each Object.entries($store.scope) as [variable, value]}
-                <NumberInput
-                  {value}
-                  on:input={(e) => formula.setScope(variable, e.target.value)}
-                  on:change={(e) => formula.setScope(variable, e.detail)}
-                />
-              {/each}
-            </FormGroup>
-          </Form>
-          <!--        <br />-->
-          <!--        <Accordion>-->
-          <!--          <AccordionItem title="Advanced">-->
-          <!--            <NumberInput label="Iterations" value={iterations} />-->
-          <!--          </AccordionItem>-->
-          <!--        </Accordion>-->
-        </Column>
+        <TextInput
+          id="formula"
+          labelText="Formula"
+          invalidText="Your formula seems invalid"
+          placeholder="2 + 2"
+          value={$store.formula}
+          on:input={(e) => formula.setFormula(e.target.value)}
+        />
+        <Tile style="display: flex; justify-content: center; align-items: center">
+          {@html $store.latex}
+        </Tile>
+        <br />
+        <Form>
+          <FormGroup legendText="Variables {Object.keys($store.scope).join()}">
+            {#each Object.entries($store.scope) as [variable, value]}
+              <NumberInput
+                step="0.01"
+                {value}
+                on:input={(e) => formula.setScope(variable, e.target.value)}
+                on:change={(e) => formula.setScope(variable, e.detail)}
+              />
+            {/each}
+          </FormGroup>
+        </Form>
+        <!--        <br />-->
+        <!--        <Accordion>-->
+        <!--          <AccordionItem title="Advanced">-->
+        <!--            <NumberInput label="Iterations" value={iterations} />-->
+        <!--          </AccordionItem>-->
+        <!--        </Accordion>-->
       {/if}
-    </Row>
-  </Grid>
+    </div>
+  </div>
 </Content>
