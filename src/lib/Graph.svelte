87b4ec644d804034b1a4bc5de3d9d96066f5a06b
Philip Breuer <breuephi@students.zhaw.ch>
2023-03-09 22:14:19 +0100

    Track center point in graph

--- src/lib/Graph.svelte
+++ src/lib/Graph.svelte
@@ -6,20 +6,18 @@
   export let formula = 'sin(x)';
   export let resolution = 100;
   export let randomize = true;
-  export let transparent = true;
   export let point = new Vector4(0, 0, 0, 1);
   export let color = new Color(0xffffff);
 
   let geometry = new IndexedBufferGeometry();
-  let material = new ShaderMaterial();
+  let material = new ShaderMaterial({ transparent: true });
   let mesh = new Points(geometry, material);
 
   $: {
-    geometry.length = resolution ** 3;
+    geometry.length = resolution * resolution;
     mesh = mesh;
   }
   $: {
-    material.transparent = transparent;
     material.uniforms.resolution = { value: resolution };
     material.uniforms.randomize = { value: randomize };
     material.uniforms.point = { value: point };
@@ -32,6 +30,8 @@
       uniform bool randomize;
       uniform int resolution;
 
+      varying vec3 pos;
+
       float rand(vec2 seed) {
         return fract(sin(dot(seed, vec2(12.9898, 78.233))) * 43758.5453);
       }
@@ -43,29 +43,33 @@
       }
 
       void main() {
-        float s = float(gl_VertexID) / float(resolution);
-        float i = 0.0;
-        float x = rand(vec2(s, i));
-        vec3 position = vec3(
-          x,
-          0.0,
-          rand(vec2(s, i + 0.1))
-        );
+        float i = float(gl_VertexID);
+        float l = float(resolution);
+        vec3 position = vec3(i / l, 0.0, mod(i, l)) / float(resolution);
+        if (randomize) {
+          position += rand(position.xy) * (1.0 / float(resolution));
+        }
         position = position * point.w + point.xyz - point.w / 2.0;
         position.y = f(position);
-        if (position.y > point.w / 2.0 || position.y < -point.w / 2.0) {
-          return;
-        }
 
         gl_PointSize = 1.0;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4((position - point.xyz), 1.0);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        pos = position;
       }
     `;
     material.fragmentShader = `
+      uniform vec4 point;
       uniform vec3 color;
+      uniform int resolution;
 
+      varying vec3 pos;
+
+      // https://www.shadertoy.com/view/Wt2yDG
+      // -abs(sin(x) * cos(y) * exp(abs(1.0 - (sqrt(x*x + y*y)/3.14159265))))
       void main() {
-        gl_FragColor = vec4(color, 1.0);
+        // float y = pos.y - point.y;
+        // float b = clamp(y, 0.0, 1.0);
+        gl_FragColor = vec4(color, 250.0 / float(resolution));
       }
     `;
     material.needsUpdate = true;
--- src/routes/formula/+page.svelte
+++ src/routes/formula/+page.svelte
@@ -1,13 +1,51 @@
 <script>
-  import { Canvas, MeshInstance, OrbitControls, OrthographicCamera, PerspectiveCamera } from '@threlte/core';
+  import {
+    Canvas,
+    MeshInstance,
+    OrbitControls,
+    OrthographicCamera,
+    PerspectiveCamera,
+  } from '@threlte/core';
   import Graph from '$lib/Graph.svelte';
   import { Grid } from '@threlte/extras';
-  import { Vector4 } from 'three';
+  import {
+    BoxBufferGeometry,
+    BoxGeometry,
+    EdgesGeometry,
+    LineBasicMaterial,
+    LineSegments,
+    Mesh,
+    Vector4,
+  } from 'three';
 
   let formula = `(2.0 * y) / (x * x + y * y)`;
   let randomize = true;
   let resolution = 100;
   let point = new Vector4(0, 0, 0, 1);
+  let controls;
+  let camera;
+
+  let box = new LineSegments(
+    new EdgesGeometry(new BoxBufferGeometry(1, 1, 1)),
+    new LineBasicMaterial({ opacity: 0.2, transparent: true }),
+  );
+  $: {
+    box.position.set(point.x, point.y, point.z);
+    box.scale.set(point.w, point.w, point.w);
+  }
+
+  $: if (controls && camera) {
+    point.w = camera.position.distanceTo(controls.target) / 2;
+  }
+
+  function change() {
+    point = point.set(
+      controls.target.x,
+      controls.target.y,
+      controls.target.z,
+      camera.position.distanceTo(controls.target) / 2,
+    );
+  }
 </script>
 
 <svelte:head>
@@ -15,49 +53,55 @@
 </svelte:head>
 
 <div class="overflow-hidden w-full h-full min-h-screen">
-  <label>
-    <input
-      type="text"
-      class="bg-shark px-2 py-1 font-mono text-xs focus:border-malibu rounded border-2 border-shark-400  transition-colors outline-none"
-      bind:value={formula}
-    />
-  </label>
-  <label>
-    randomize
-    <input
-      type="checkbox"
-      class="bg-shark px-2 py-1 font-mono text-xs focus:border-malibu rounded border-2 border-shark-400  transition-colors outline-none"
-      bind:checked={randomize}
-    />
-  </label>
-  <label>
-    resolution
-    <input
-      type="range"
-      class="bg-shark px-2 py-1 font-mono text-xs focus:border-malibu rounded border-2 border-shark-400  transition-colors outline-none"
-      bind:value={resolution}
-    />
-  </label>
-  <label>
-    size
-    <input
-      type="range"
-      class="bg-shark px-2 py-1 font-mono text-xs focus:border-malibu rounded border-2 border-shark-400  transition-colors outline-none"
-      bind:value={point.w}
-    />
-  </label>
+  <div class="fixed">
+    <label>
+      <input
+        type="text"
+        class="bg-shark px-2 py-1 font-mono text-xs focus:border-malibu rounded border-2 border-shark-400  transition-colors outline-none"
+        bind:value={formula}
+      />
+    </label>
+    <label>
+      randomize
+      <input
+        type="checkbox"
+        class="bg-shark px-2 py-1 font-mono text-xs focus:border-malibu rounded border-2 border-shark-400  transition-colors outline-none"
+        bind:checked={randomize}
+      />
+    </label>
+    <label>
+      resolution: {resolution}
+      <br />
+      <input
+        type="range"
+        class="bg-shark px-2 py-1 font-mono text-xs focus:border-malibu rounded border-2 border-shark-400  transition-colors outline-none"
+        bind:value={resolution}
+        min="10"
+        max="10000"
+      />
+    </label>
+  </div>
 
   <Canvas>
     <!-- <OrthographicCamera position={{ x: 0, y: 0, z: 3 }} lookAt={{ x: 0, y: 0, z: 0 }} zoom="500">
       <OrbitControls />
     </OrthographicCamera> -->
 
-    <PerspectiveCamera fov={60} position={{ x: 3, y: 3, z: 3 }} lookAt={{ x: 0, y: 0, z: 0 }}>
-      <OrbitControls screenSpacePanning />
+    <PerspectiveCamera fov={60} position={{ x: 3, y: 3, z: 3 }} bind:camera>
+      <OrbitControls screenSpacePanning on:change={change} bind:controls />
     </PerspectiveCamera>
 
+    <MeshInstance mesh={box} />
+
     <!--    <MeshInstance mesh={new AxesHelper(10000000)} />-->
-    <Grid infiniteGrid fadeDistance={100} />
+    <!--    <Grid-->
+    <!--      infiniteGrid-->
+    <!--      fadeDistance={100}-->
+    <!--      sectionColor="#111"-->
+    <!--      sectionThickness={1}-->
+    <!--      cellThickness={0.5}-->
+    <!--      followCamera={false}-->
+    <!--    />-->
 
     <Graph {formula} {randomize} {resolution} {point} />
   </Canvas>
