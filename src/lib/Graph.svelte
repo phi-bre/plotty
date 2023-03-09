1850382c50769dde89c64e8bbdfd59d6e1ebc2ca
Philip Breuer <philip.breuer@pm.me>
2023-03-09 09:22:12 +0100

    Copy mandeldot implementation for graph

--- src/lib/Graph.svelte
+++ src/lib/Graph.svelte
@@ -1,58 +1,74 @@
 <script>
-  import { MeshInstance, useThrelte } from '@threlte/core';
-  import { Line, ShaderMaterial, Vector4 } from 'three';
+  import { Points, ShaderMaterial, Color, Vector4 } from 'three';
+  import { MeshInstance } from '@threlte/core';
   import { IndexedBufferGeometry } from '$lib/utils.js';
 
   export let formula = 'sin(x)';
-  export let resolution;
-  export let mode = 'lines';
+  export let resolution = 100;
+  export let randomize = true;
+  export let transparent = true;
+  export let point = new Vector4(0, 0, 0, 1);
+  export let color = new Color(0xffffff);
 
-  // const { size } = useThrelte();
-  // $: resolution = resolution ?? $size.width;
+  let geometry = new IndexedBufferGeometry();
+  let material = new ShaderMaterial();
+  let mesh = new Points(geometry, material);
 
-  const geometry = new IndexedBufferGeometry(1000);
-  // $: geometry.length = resolution; TODO
-
-  $: material = new ShaderMaterial({
-    uniforms: {
-      resolution: { value: 1000 },
-    },
-    vertexShader: `
+  $: {
+    geometry.length = resolution ** 3;
+    mesh = mesh;
+  }
+  $: {
+    material.transparent = transparent;
+    material.uniforms.resolution = { value: resolution };
+    material.uniforms.randomize = { value: randomize };
+    material.uniforms.point = { value: point };
+    material.uniforms.color = { value: color };
+    mesh = mesh;
+  }
+  $: {
+    material.vertexShader = `
+      uniform vec4 point;
+      uniform bool randomize;
       uniform int resolution;
 
+      float rand(vec2 seed) {
+        return fract(sin(dot(seed, vec2(12.9898, 78.233))) * 43758.5453);
+      }
+
+      float f(vec3 voxel) {
+        float x = voxel.x;
+        float y = voxel.z;
+        return (${formula});
+      }
+
       void main() {
-        float near = projectionMatrix[3][2] / (projectionMatrix[2][2] - 1.0);
-        float left = near * (projectionMatrix[2][0] - 1.0) / projectionMatrix[0][0];
-        float x = float(gl_VertexID) * (left / float(resolution) * 2.0) + cameraPosition.x;
+        float error = 2.0 / float(resolution);
 
-        // float error = 2.0 / float(resolution);
-        // vec3 position = vec3(float(gl_VertexID) / float(resolution), 0, 0) * error - 1.0;
-        // float x = position.x;
+        vec3 position = vec3(
+          gl_VertexID % resolution,
+          gl_VertexID / resolution % resolution,
+          gl_VertexID / resolution / resolution % resolution
+        ) * error - 1.0;
 
-        float y = ${formula};
+        vec3 voxel = position * point.w + point.xyz;
+        if (randomize) voxel += (error * 2.0) * (rand(voxel.xy) - 1.0);
+        voxel.y = f(voxel);
 
         gl_PointSize = 1.0;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(x, y, 0, 1.0);
+        gl_Position = projectionMatrix * modelViewMatrix * vec4((voxel - point.xyz) / point.w, 1.0);
       }
-    `,
-    fragmentShader: `
-      uniform vec4 color;
+    `;
+    material.fragmentShader = `
+      uniform vec3 color;
 
       void main() {
-        gl_FragColor = vec4(0.365, 0.678, 1, 0);
+        gl_FragColor = vec4(color, 1.0);
       }
-    `,
-  });
-
-  // $: {
-  //   material.uniforms.resolution = resolution;
-  // }
+    `;
+    material.needsUpdate = true;
+    mesh = mesh;
+  }
 </script>
 
-<!--{#if mode === 'points'}-->
-<!--  <MeshInstance frustumCulled={false} mesh={new Points(geometry, material)} />-->
-<!--{:else if mode === 'lines'}-->
-<!--  <Line {geometry} {material} />-->
-<!--{/if}-->
-<!--<Line {geometry} {material} />-->
-<MeshInstance frustumCulled={false} mesh={new Line(geometry, material)} />
+<MeshInstance frustumCulled={false} {mesh} />
